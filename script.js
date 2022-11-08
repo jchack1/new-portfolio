632e261874c7e9bf0db31260b9574ef17c5c29d0
jchack1 <55161597+jchack1@users.noreply.github.com>
2022-11-07 22:06:36 -0700

    styling updates - sticky nav

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const hamburgerMenu = document.querySelector("#hamburger-menu");
 const mobileMenu = document.querySelector("#global-nav");
 const closeMenu = document.querySelector("#close-menu");
 
-hamburgerMenu.addEventListener("click", function(){
-    mobileMenu.className = "menu-open";
-})
+hamburgerMenu.addEventListener("click", function () {
+  mobileMenu.className = "menu-open";
+});
 
-closeMenu.addEventListener("click", function(){
-    mobileMenu.className = "menu-close";
-})
+closeMenu.addEventListener("click", function () {
+  mobileMenu.className = "menu-close";
+});
